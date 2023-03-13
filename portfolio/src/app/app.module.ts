98d030a2dd3a53e16e82d1837a3501d3d08ff405
jolanta.pisulska <jolanta.pisulska@gmail.com>
2023-03-13 18:46:45 +0100

    add project structure

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './profile/about/about.component';
+import { HeaderComponent } from './profile/header/header.component';
+import { HomeComponent } from './profile/home/home.component';
+import { ProjectsComponent } from './profile/projects/projects.component';
+import { SkillsComponent } from './profile/skills/skills.component';
+import { ContactComponent } from './profile/contact/contact.component';
+import { FooterComponent } from './profile/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
